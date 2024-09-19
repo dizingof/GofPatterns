93018ed1a5f1b94a3eb923b6e676ca9b5a8aeea2
DESKTOP-KUSCP3I\roman <romannep1989@gmail.com>
2024-09-19 14:16:20 +0300

    add prototype

---
+++ Prototype/Car.cs
@@ -0,0 +1,23 @@
+﻿public class Car : IPrototype<Car>
+{
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int HorsePower { get; set; }
+
+    public Car(string model, string color, int horsePower)
+    {
+        Model = model;
+        Color = color;
+        HorsePower = horsePower;
+    }
+
+    public Car Clone()
+    {
+        return (Car)this.MemberwiseClone();
+    }
+
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Car Model: {Model}, Color: {Color}, HorsePower: {HorsePower}");
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,10 @@
+﻿Car originalCar = new Car("Tesla Model S", "Red", 670);
+Console.WriteLine("Original Car:");
+originalCar.ShowDetails();
+
+Car clonedCar = originalCar.Clone();
+clonedCar.Color = "Blue"; /
+Console.WriteLine("\nCloned Car:");
+clonedCar.ShowDetails();
+
+Console.ReadLine();
